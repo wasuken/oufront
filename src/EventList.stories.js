6bf902ba4908e9832a07719098b36948244de7ce
wasuken <wasuken1@gmail.com>
2021-12-20 19:45:39 +0900

    added jest and cypress and storybook.

---
+++ src/EventList.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {action} from '@storybook/addon-actions';
+import { EventList } from './components/EventList';
+import { OufEvent, EventListProps, initialOufEventState } from './types'
+
+export default {
+  title: 'eventList'
+};
+
+const eList: OufEvent[] = [
+  {
+    title: "sample",
+    description: `これはテスト
+テストこれは
+ててててこれはすと`,
+    event_date: {
+      begin: new Date(),
+      end: new Date(),
+    },
+    participate_date: {
+      begin: new Date(),
+      end: new Date(),
+    },
+  },
+  {
+    title: "sample2",
+    description: "buriburiburyu",
+    event_date: {
+      begin: new Date(),
+      end: new Date(),
+    },
+    participate_date: {
+      begin: new Date(),
+      end: new Date(),
+    },
+  },
+];
+
+export const eventList = () => (
+  <EventList list={eList} />
+);
---
+++ src/Login.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {action} from '@storybook/addon-actions';
+import { Login } from './containers/Login';
+
+export default {
+  title: 'login'
+};
+
+export const LoginPage = () => (
+  <Login />
+);
